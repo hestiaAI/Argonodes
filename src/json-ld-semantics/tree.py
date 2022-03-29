82dfebb8d7a3c77e2c6070b27ea40e1effee07d3
Stache <hugo.hueber@epfl.ch>
2022-03-29 19:15:45 +0200

    WIP

--- src/json-ld-semantics/tree.py
+++ src/json-ld-semantics/tree.py
@@ -1,10 +1,12 @@
+from typing import Optional
+
 class Root:
     def __str__(self):
         return "RootNode"
 
 
 class Node:
-    def __init__(self, fieldName, data, parent=None, process_traversal=True, process_children=False):
+    def __init__(self, fieldName, data, parent=None, process_traversal=True, process_children=True):
         self.fieldName = fieldName
         self.data = data
         self.foundType = Root if self.fieldName == "$" else type(data)
@@ -17,6 +19,7 @@ class Node:
         self.parent = parent
         self.traversal = {}
         self.children = []
+        self.path = self.set_path()
 
         self.process(traversal=process_traversal, children=process_children)
 
@@ -24,43 +27,76 @@ class Node:
         return repr(self)
 
     def __repr__(self) -> str:
-        return f"{type(self).__name__} '{self.fieldName}' with {len(self.children)} children{'s' if len(self.children) != 1 else ''}"
+        rep = f"{type(self).__name__} '{self.fieldName}' with {len(self.children)} children{'s' if len(self.children) != 1 else ''}\n"
+        for attr in self.get_attributes():
+            if attr in ["data", "children"]:
+                rep += f"- {attr}: Length of {len(getattr(self, attr))}\n"
+            elif attr in ["traversal"]:
+                continue
+            elif attr in ["foundType"]:
+                rep += f"- {attr}: {getattr(self, attr).__name__}\n"
+            elif attr in ["parent"]:
+                rep += f"- {attr}: {getattr(self, attr).fieldName if getattr(self, attr) else None}\n"
+            else:
+                rep += f"- {attr}: {getattr(self, attr)}\n"
+        return rep
 
-    def path(self) -> str:
+    def set_path(self) -> str:
         if not self.parent:
             return self.fieldName
         name = f".{self.fieldName}" if self.fieldName != "[*]" else self.fieldName
-        return self.parent.path() + name
+        return self.parent.path + name
 
-    def all_paths(self, level=0) -> str:
-        ret = "  " * level + self.path() + "\n"
+    def get_all_paths(self, level=0) -> str:
+        ret = "  " * level + self.path + "\n"
         if self.traversal:
             for key, children in self.traversal.items():
-                ret += children.all_paths(level + 1)
+                ret += children.get_all_paths(level + 1)
         return ret
 
-    def process(self, traversal=True, children=False) -> None:
+    def process(self, traversal, children) -> None:
         if isinstance(self.data, dict):
             for key, children in self.data.items():
                 if traversal:
-                    self.traversal[key] = NodeDict(key, children, parent=self)
+                    self.traversal[key] = NodeDict(key, children, parent=self, process_traversal=traversal,
+                                                   process_children=children)
                 if children:
-                    self.children.append(NodeDict(key, children, parent=self))
+                    self.children.append(
+                        NodeDict(key, children, parent=self, process_traversal=traversal, process_children=children))
         elif isinstance(self.data, list):
-            for children in self.data:
+            for i, children in enumerate(self.data):
                 if traversal:
-                    self.traversal["[*]"] = NodeList(children, parent=self)
+                    self.traversal["[*]"] = NodeList(children, parent=self, process_traversal=traversal,
+                                                     process_children=children)
                 if children:
-                    self.children.append(NodeList(children, parent=self))
+                    self.children.append(
+                        NodeList(children, i=i, parent=self, process_traversal=traversal, process_children=children))
         else:
             return
 
+    def get_attributes(self) -> list:
+        return list(self.__dict__.keys())
+
+    def get_children_from_path(self, path) -> Optional[Node]:
+        if not self.children:
+            return None
+        if self.path not in path:
+            return None
+        if self.path == path:
+            return self
+        else:
+            for children in self.children:
+                if children.path in path:
+                    return children.get_children_from_path(path)
+
 
 class NodeList(Node):
-    def __init__(self, contains, parent=None):
-        super().__init__("[*]", contains, parent=parent)
+    def __init__(self, contains, parent, process_traversal, process_children, i=None):
+        super().__init__(f"[{i}]" if i != None else "[*]", contains, parent=parent, process_traversal=process_traversal,
+                         process_children=process_children)
 
 
 class NodeDict(Node):
-    def __init__(self, fieldName, contains, parent):
-        super().__init__(fieldName, contains, parent=parent)
+    def __init__(self, fieldName, contains, parent, process_traversal, process_children):
+        super().__init__(fieldName, contains, parent=parent, process_traversal=process_traversal,
+                         process_children=process_children)
