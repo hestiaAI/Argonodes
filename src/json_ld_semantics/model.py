a286a5f0cbc56e5238a51f4fc724b747e701d36d
Stache <hugo.hueber@epfl.ch>
2022-04-05 14:07:16 +0200

    Clean semantics

--- src/json_ld_semantics/model.py
+++ src/json_ld_semantics/model.py
@@ -1,3 +1,6 @@
+"""
+model.py is everything linked to a model or abstraction of one or multiple files.
+"""
 from typing import Optional
 import json
 import re
@@ -10,17 +13,22 @@ from .semantics import Tree
 
 
 def parse_path(path):
+    """
+    Parse a JSON path into a list.
+    :param path: String, a JSON path.
+    :return: List, parsed JSON path.
+    """
     return [r for r in re.split("\.|(\[\*\])", path) if r]
 
 
 class Model:
     """
-    Model for a data frame.
+    Model for a specific type of data.
     Internal: Python Dict.
-    External: Either JSon or a String.
+    External: Either JSON or a String.
     """
 
-    def __init__(self, context=None, filenames=None, traversal=None, frame=None, multiple=True):
+    def __init__(self, context=None, filenames=None, traversal=None, frame=None):
         if context:
             self.context = context
         else:
@@ -41,15 +49,18 @@ class Model:
         else:
             self.frame = {}
 
-        self.multiple = multiple
-
     def add_files(self, filenames) -> int:
+        """
+        Add files to parse into the model.
+        :param filenames: String or List[String], file paths.
+        :return: Number of files effectively added.
+        """
         if not isinstance(filenames, list):
             filenames = [filenames]
 
         for filename in filenames:
             try:
-                with open(filename, "r"):  # This is for checking the file exists.
+                with open(filename, "r", encoding="utf-8"):  # This is for checking the file exists.
                     self.filenames.append(filename)
             except FileNotFoundError:
                 print(f"Warning: {filename} could not be opened.")
@@ -57,18 +68,28 @@ class Model:
         return len(self.filenames)
 
     def remove_files(self, filenames) -> int:
+        """
+        Remove files to parse into the model.
+        :param filenames: String or List[String], file paths.
+        :return: Number of files still remaining.
+        """
         if not isinstance(filenames, list):
             filenames = [filenames]
 
         for filename in filenames:
             try:
                 self.filenames.remove(filename)
-            except ValueError as e:
+            except ValueError:
                 print(f"Warning: {filename} was not found in the list.")
 
         return len(self.filenames)
 
     def process_files(self, apply=True) -> list:
+        """
+        Process each file and add to the model traversal.
+        :param apply: If True, changes are directly applied to the model; else, changes are not applied.
+        :return: List of changes.
+        """
         if apply:
             full_traversal = self.traversal
         else:
@@ -76,8 +97,8 @@ class Model:
         changes = []
 
         for filename in self.filenames:
-            with open(filename, "r") as f:
-                json_data = json.load(f)
+            with open(filename, "r", encoding="utf-8") as file:
+                json_data = json.load(file)
 
             traversal = Tree(json_data).export_traversal()
             changes.append((filename, DeepDiff(full_traversal, traversal)))
@@ -86,18 +107,23 @@ class Model:
         return changes
 
     def get_paths(self) -> set:
+        """
+        Returns the set of avalaible paths.
+        :return: Set[String], set of paths avalaible.
+        """
+
         def recur(traversal):
-            for key, info in traversal.items():
+            for _, info in traversal.items():
                 yield info["path"]
                 yield from recur(info["traversal"])
 
         return set(recur(self.traversal))
 
-    def find_info(self, path):
+    def find_info(self, path) -> dict:
         """
         Returns the given path, or the latest found element within the path.
         :param path: String, a JSON path.
-        :return: The corresponding linked information in the traversal.
+        :return: Dict, the corresponding linked information in the traversal.
         """
 
         def recur(traversal, liste):
@@ -112,12 +138,17 @@ class Model:
         return recur(self.traversal, parse_path(path))
 
     def to_list(self, headers=True) -> list:
+        """
+        Returns the model in the form of a list.
+        :param headers: If the first line should be the headers.
+        :return: List[List].
+        """
         rtn = []
         if headers:
             rtn.append(["path", "foundType", "descriptiveType", "unique", "default", "description", "example", "regex"])
 
         def recur(traversal):
-            for key, info in traversal.items():
+            for _, info in traversal.items():
                 yield [
                     info["path"],
                     info["foundType"],
@@ -134,10 +165,19 @@ class Model:
 
         return rtn
 
-    def set_attribute(self, path, **kwargs) -> None:
+    def set_attribute(self, path, **kwargs) -> bool:
+        """
+        Given a specific path, add more context to that path.
+        :param path: String, a valide path.
+        :param kwargs:
+        :return: True if the path was found and info added; False otherwise.
+        """
         info = self.find_info(path)
-        for k, v in kwargs.items():
-            info[k] = v
+        if info:
+            for attr, value in kwargs.items():
+                info[attr] = value
+            return True
+        return False
 
     # def apply_self_traversal(self):
     #     self.frame = get_extended_traversal(traversal=self.traversal)
@@ -152,6 +192,6 @@ class Model:
         if text:
             return json.dumps(self._frame_and_context())
         elif filename:
-            with open(filename, "w") as file:
-                json.dump(self._frame_and_context(), filename, indent=4)
+            with open(filename, "w", encoding="utf-8") as file:
+                json.dump(self._frame_and_context(), file, indent=4)
             return
