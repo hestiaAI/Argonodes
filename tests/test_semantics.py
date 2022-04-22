c2182666f3c162a07301f8f166d687ca4e56cf02
Stache <stache@stache.cat>
2022-04-22 16:23:37 +0200

    Test CI

---
+++ tests/test_semantics.py
@@ -0,0 +1,25 @@
+import json
+
+
+import pytest
+
+
+from json_ld_semantics.semantics import Tree
+
+
+@pytest.fixture
+def tree_github():
+    with open("./inputs/pull_requests_000001.json7", "r") as jsonfile:
+        json_data = json.loads(jsonfile.read())
+        return Tree(json_data)
+
+
+@pytest.fixture
+def tree_google():
+    with open("./inputs/2022_MARCH.json", "r") as jsonfile:
+        json_data = json.loads(jsonfile.read())
+        return Tree(json_data)
+
+
+def test_sample():
+    assert True
