b4b47f053327f0607a9a965a9e0ebfab42e8293a
Stache <stache@stache.cat>
2022-05-18 17:15:31 +0200

    Cleaning

--- src/argonodes/appliers.py
+++ src/argonodes/appliers.py
@@ -14,86 +14,6 @@ import uuid
 from .helpers import flatten, REGEX_PATH, REGEX_SEARCH
 
 
-def base_apply(node, rec=True, *args, **kwargs) -> None:
-    """
-    This base method is an example and should not be used.
-
-    :param node: A given Node, usually a Tree.
-    :type node: Node
-    :param rec: If True, the function shall be applied on all children.
-    :type rec: bool, default True.
-    :param args: Add. args.
-    :param kwargs: Add. kwargs.
-    :return: Nothing, because applies should be chained.
-    """
-
-    def apply_to(node):
-        pass
-
-    def recur(node):
-        apply_to(node)
-        if node.children:
-            for children in node.children:
-                recur(children)
-
-    if rec:
-        recur(node)
-    else:
-        apply_to(node)
-
-    return
-
-
-def make_traversal(node, rec=True) -> None:
-    """
-    Create the base traversal of a Node, usually a Tree.
-
-    :param node: A given Node, usually a Tree.
-    :type node: Node
-    :param rec: Must be True.
-    :type rec: bool, default True.
-    """
-    if not rec:
-        raise AssertionError("Cannot make traversal not recursively.")
-
-    void = {}
-
-    def recur(node, void) -> None:
-        ignored = hasattr(node, "ignore") and node.ignore
-
-        path = REGEX_PATH.sub("[*]", node.path)
-        if ignored:
-            path = uuid.uuid5(uuid.NAMESPACE_URL, path)
-
-        if path not in void:
-            if ignored:
-                void[path] = {
-                    "traversal": {},
-                }
-            else:
-                void[path] = {
-                    "foundType": node.foundType,
-                    "descriptiveType": node.descriptiveType,
-                    "unique": node.unique,
-                    "default": node.default,
-                    "description": node.description,
-                    "choices": node.choices,
-                    "regex": node.regex,
-                    "traversal": {},
-                }
-
-        if node.children:
-            for children in node.children:
-                recur(children, void[path]["traversal"])
-        else:
-            return
-
-    recur(node, void)
-    node.traversal = void
-
-    assert flatten(node.traversal) == node.get_paths()  # Move to testing in next iteration
-
-
 class DistinctValues:
     """
     Standalone Applier to check all distinct values for Nodes.
@@ -134,6 +54,12 @@ class DistinctValues:
 
     @property
     def data(self) -> dict:
+        """
+        Raw data. Immutable.
+
+        :return: Raw data.
+        :rtype: dict
+        """
         srt = 1 if self.sort == "count" else 0
         rvrs = self.reverse or True if self.sort == "count" else False
 
@@ -196,42 +122,6 @@ class DistinctValues:
         ]
 
 
-def ignore_node(node, rec=False, paths=None) -> None:
-    """
-    Marks every targeted Node as "ignored" - subsequent functions shall take that into account for further actions (e.g., export).
-
-    :param node: A given Node, usually a Tree.
-    :type node: Node
-    :param rec: If True, the function shall be applied on all children.
-    :type rec: bool, default True.
-    :param paths: What are the targeted paths.
-    :type paths: List[str] or str.
-    """
-    if paths and not isinstance(paths, list):
-        paths = [paths]
-
-    def apply_to(node):
-        if paths:
-            for path in paths:
-                if REGEX_SEARCH(path).match(node.path):
-                    node.ignore = True
-        else:
-            node.ignore = True
-
-    def recur(node):
-        apply_to(node)
-        if node.children:
-            for children in node.children:
-                recur(children)
-
-    if paths and rec:
-        recur(node)
-    else:
-        apply_to(node)
-
-    return
-
-
 class FoundRegex:
     """
     This standalone Applier will try to find a matching regex for each targeted Node.
@@ -275,8 +165,100 @@ class FoundRegex:
 
     @property
     def data(self) -> dict:
+        """
+        Raw data. Immutable.
+
+        :return: Raw data.
+        :rtype: dict
+        """
         return self._data
 
     @data.setter
     def data(self, value) -> None:
         raise AttributeError("Cannot set `data`.")
+
+
+def make_traversal(node, rec=True) -> None:
+    """
+    Create the base traversal of a Node, usually a Tree.
+
+    :param node: A given Node, usually a Tree.
+    :type node: Node
+    :param rec: Must be True.
+    :type rec: bool, default True.
+    """
+    if not rec:
+        raise AssertionError("Cannot make traversal not recursively.")
+
+    void = {}
+
+    def recur(node, void) -> None:
+        ignored = hasattr(node, "ignore") and node.ignore
+
+        path = REGEX_PATH.sub("[*]", node.path)
+        if ignored:
+            path = uuid.uuid5(uuid.NAMESPACE_URL, path)
+
+        if path not in void:
+            if ignored:
+                void[path] = {
+                    "traversal": {},
+                }
+            else:
+                void[path] = {
+                    "foundType": node.foundType,
+                    "descriptiveType": node.descriptiveType,
+                    "unique": node.unique,
+                    "default": node.default,
+                    "description": node.description,
+                    "choices": node.choices,
+                    "regex": node.regex,
+                    "traversal": {},
+                }
+
+        if node.children:
+            for children in node.children:
+                recur(children, void[path]["traversal"])
+        else:
+            return
+
+    recur(node, void)
+    node.traversal = void
+
+    assert flatten(node.traversal) == node.get_paths()  # Move to testing in next iteration
+
+
+def ignore_node(node, rec=False, paths=None) -> None:
+    """
+    Marks every targeted Node as "ignored" - subsequent functions shall take that into account for further actions (e.g., export).
+
+    :param node: A given Node, usually a Tree.
+    :type node: Node
+    :param rec: If True, the function shall be applied on all children.
+    :type rec: bool, default True.
+    :param paths: What are the targeted paths.
+    :type paths: List[str] or str.
+    """
+    if paths and not isinstance(paths, list):
+        paths = [paths]
+
+    def apply_to(node):
+        if paths:
+            for path in paths:
+                if REGEX_SEARCH(path).match(node.path):
+                    node.ignore = True
+        else:
+            node.ignore = True
+
+    def recur(node):
+        apply_to(node)
+        if node.children:
+            for children in node.children:
+                recur(children)
+
+    if paths and rec:
+        recur(node)
+    else:
+        apply_to(node)
+
+    return
