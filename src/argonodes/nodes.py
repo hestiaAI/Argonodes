8ef466fc1caf4dc4d27269ef84645659b7689ae3
Stache <stache@stache.cat>
2022-06-19 22:22:28 +0200

    Add filename

--- src/argonodes/nodes.py
+++ src/argonodes/nodes.py
@@ -9,6 +9,7 @@ from __future__ import annotations
 
 
 from typing import Union
+import os
 import uuid
 
 
@@ -17,7 +18,7 @@ from .helpers import REGEX_PATH, REGEX_SEARCH
 
 
 MAX_DATA = 128
-PROTECTED_ATTRS = ["fieldName", "data", "foundType", "parent", "children", "path"]
+PROTECTED_ATTRS = ["fieldName", "filename", "data", "foundType", "parent", "children", "path"]
 
 
 class Root:
@@ -52,14 +53,17 @@ class Node:
     :type data: Any
     :param fieldName: The field name for that Node in the raw JSON data.
     :type fieldName: String
+    :param filename: Facultative, what file is the data from.
+    :type filename: String, default None.
     :param parent: Facultative, parent for that Node.
     :type parent: Node, default None.
     :param process_traversal: Facultative, whether a traversal should be processed already or not.
     :type process_traversal: bool, default False.
     """
 
-    def __init__(self, data, fieldName, parent=None, process_traversal=False) -> None:
+    def __init__(self, data, fieldName, filename=None, parent=None, process_traversal=False) -> None:
         self.fieldName = fieldName
+        self.filename = os.path.basename(filename) if filename else None
         self.foundType = Root if self.fieldName == "$" else type(data)
         self.descriptiveType = None
         self.unique = None
@@ -70,9 +74,10 @@ class Node:
         self.parent = parent
         self.traversal = {}
         self.children = []
-        self.path = self._set_path()
+        self._path = self._set_path()
+        self.path = f"{self.filename}:{self._path}" if self.filename else self._path
 
-        self._process(data, process_traversal=process_traversal)
+        self._process(data, process_traversal=process_traversal, filename=filename)
 
     def __str__(self) -> str:
         return repr(self)
@@ -87,7 +92,7 @@ class Node:
             rep += f" with {len(self.get_paths())} path{'s' if len(self.get_paths()) != 1 else ''}"
         rep += "\n"
         for attr in self.get_attributes():
-            if attr in ["children", "traversal"]:
+            if attr in ["children", "traversal", "_path"]:
                 # Skip
                 continue
             elif attr in ["data"] and getattr(self, attr):
@@ -122,7 +127,7 @@ class Node:
         if not self.parent:
             return self.fieldName
         name = f".{self.fieldName}" if not REGEX_PATH.match(self.fieldName) else self.fieldName
-        return self.parent.path + name
+        return self.parent._path + name
 
     def get_paths(self) -> set:
         """
@@ -156,7 +161,7 @@ class Node:
 
         return "\n".join(sorted(set(r for r in recur(self)), key=lambda x: x.strip()))
 
-    def _process(self, data, process_traversal) -> None:
+    def _process(self, data, process_traversal, filename=None) -> None:
         """
         Internal, create the hierarchy for that Node.
 
@@ -169,12 +174,16 @@ class Node:
             self.data = None
             for key, children in data.items():
                 self.children.append(
-                    NodeDict(children, fieldName=key, parent=self, process_traversal=process_traversal)
+                    NodeDict(
+                        children, fieldName=key, filename=filename, parent=self, process_traversal=process_traversal
+                    )
                 )
         elif isinstance(data, list):
             self.data = None
             for i, children in enumerate(data):
-                self.children.append(NodeList(children, i=i, parent=self, process_traversal=process_traversal))
+                self.children.append(
+                    NodeList(children, i=i, filename=filename, parent=self, process_traversal=process_traversal)
+                )
         else:
             self.data = data
 
@@ -216,6 +225,11 @@ class Node:
         """
         if not isinstance(paths, list):
             paths = [paths]
+
+        if self.filename:
+            for i, path in enumerate(paths):
+                if ":" not in path:
+                    paths[i] = f"{self.filename}:{path}"
 
         def recur(targets):
             for target in targets:
@@ -263,8 +277,8 @@ class Tree(Node):
     A Tree is a special Node with `$` as field name.
     """
 
-    def __init__(self, data):
-        super().__init__(data, fieldName="$")
+    def __init__(self, data, filename=None):
+        super().__init__(data, fieldName="$", filename=filename)
         self.unique = NA
         self.default = NA
         self.choices = NA
@@ -279,10 +293,11 @@ class NodeList(Node):
     A NodeList is a special Node that contains data in a list.
     """
 
-    def __init__(self, contains, parent, process_traversal, i=None):
+    def __init__(self, contains, parent, process_traversal, i=None, filename=None):
         super().__init__(
             contains,
             fieldName=f"[{i}]" if i is not None else "[*]",
+            filename=filename,
             parent=parent,
             process_traversal=process_traversal,
         )
@@ -293,10 +308,11 @@ class NodeDict(Node):
     A NodeDict is a special Node that contains data in a dict.
     """
 
-    def __init__(self, contains, fieldName, parent, process_traversal):
+    def __init__(self, contains, fieldName, parent, process_traversal, filename=None):
         super().__init__(
             contains,
             fieldName=fieldName,
+            filename=filename,
             parent=parent,
             process_traversal=process_traversal,
         )
