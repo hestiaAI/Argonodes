58e9c8872446a6d0209a8fd447fca9b457e4b4b4
Stache <Amustache@users.noreply.github.com>
2022-06-24 13:32:06 +0200

    More checks for docstring and versions (#78)
    * Maintenance
    
    * bump test
    
    * Add docstrings (kinda)

--- src/argonodes/parsers.py
+++ src/argonodes/parsers.py
@@ -128,6 +128,8 @@ class TwitterJSParser(JSParser):
 
 
 class ZIPParser:
+    """ """
+
     def __init__(self, parser=None, regex=None, extension=None, mime=None, verbose=None):
         self.parser = parser()
         self.regex = regex
